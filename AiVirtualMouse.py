05c9072eb3c7d6a906a1a7ccaa98742919d3fa60
Shikher-jain <shikherjain786@gmail.com>
2025-05-05 13:53:23 +0530

    AI Virtual Mouse

---
+++ AiVirtualMouse.py
@@ -0,0 +1,26 @@
+import handTrackingModule as htm
+import numpy as np
+import autopy
+import time
+import cv2
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+pTime = 0
+while True:
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv2.putText(img,f"FPS:{int(fps)}", (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
